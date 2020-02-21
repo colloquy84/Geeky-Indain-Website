2d429f539cfcc616fd6eed53ac5f792c129bbf2a
Geeky Indian <thegeekyindian84@gmail.com>
2020-02-21 21:09:04 +0530

    Changed the location

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -11,7 +11,7 @@ const HomePage = () => (
 	          <div className="masthead-content text-white py-5">
 	            <h1 className="mb-3">Coming Soon</h1>
 	            <p className="mb-5">We are working hard to finish the development of this site. Our target launch date is
-	              <strong>&nbsp;May 2020</strong>. Sign up for updates using the form below</p>
+	              <strong>&nbsp;May 2021</strong>. Sign up for updates using the form below</p>
 	            <div className="input-group input-group-newsletter">
 	              <input type="email" className="form-control" placeholder="Enter email..." aria-label="Enter email..."
 	              aria-describedby="basic-addon"/>
